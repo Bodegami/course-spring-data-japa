902bd05cef804d203484c290677c9fbba483c030
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-07-15 22:44:15 -0300

    add Entity Cargo

---
+++ springdata/src/main/java/br/com/renato/springdata/orm/Cargo.java
@@ -0,0 +1,43 @@
+package br.com.renato.springdata.orm;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cargos")
+public class Cargo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String descricao;
+
+	// JPA only
+	public Cargo() {
+	}
+
+	public Cargo(Integer id, String descricao) {
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
