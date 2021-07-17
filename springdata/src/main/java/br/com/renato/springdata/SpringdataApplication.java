19f0bc31fa1ab1ef88e7df5683ec46f72369a521
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-07-16 22:08:19 -0300

    inserindo um cargo na tabela

--- springdata/src/main/java/br/com/renato/springdata/SpringdataApplication.java
+++ springdata/src/main/java/br/com/renato/springdata/SpringdataApplication.java
@@ -1,13 +1,31 @@
 package br.com.renato.springdata;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.renato.springdata.orm.Cargo;
+import br.com.renato.springdata.repository.CargoRepository;
+
 @SpringBootApplication
-public class SpringdataApplication {
+public class SpringdataApplication implements CommandLineRunner{
+	
+	private final CargoRepository repository;
+	
+	public SpringdataApplication(CargoRepository repository) {
+		this.repository = repository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
+		repository.save(cargo);
+		
 	}
 
 }
